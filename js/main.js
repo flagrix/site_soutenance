d36db69bbfd66c38a56cc3ca14a6a5931c5c9e7e
flagrix <g.benier9@yahoo.com>
2025-01-16 21:37:03 +0100

    commit qdqed

---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿const assets = document.querySelector('.assets');
+const pageAssets = document.querySelector('.page2'); // Sélectionne un seul élément avec querySelector
+const home = document.querySelector('.home');
+const pageHome = document.querySelector('.pageHome');
+
+assets.addEventListener('click', () => {
+    pageAssets.style.display = "block";
+    pageHome.style.display = "none";
+    console.log("Page Features");
+});
+
+home.addEventListener('click', () => {
+    pageAssets.style.display = "none";
+    pageHome.style.display = "flex";
+    console.log("Home Page");
+});
